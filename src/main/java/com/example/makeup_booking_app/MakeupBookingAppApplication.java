f7927e4a61854278e4c531133aad0aedc48edf8f
Tran Kim Duyen <duyentk1655@ut.edu.vn>
2025-03-11 17:56:42 +0700

    add configuration

---
+++ src/main/java/com/example/makeup_booking_app/MakeupBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.makeup_booking_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/makeup_booking_app/MakeupBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makeup_booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeupBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
