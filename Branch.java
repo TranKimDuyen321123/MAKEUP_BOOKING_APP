b2384af1eebe8994166624e7119976e8fb9c5f4e
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-03-25 15:40:19 +0700

    Add files via upload

---
+++ Branch.java
@@ -0,0 +1,85 @@
+package com.example.makeup_booking_app;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "branches")
+public class Branch {
+    @Id
+    @Column(name = "id", nullable = false, unique = true)
+    private String id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "latitude", nullable = false) // Vi do
+    private double latitude;
+
+    @Column(name = "longitude", nullable = false) // Kinh do
+    private double longitude;
+
+    public Branch() {
+    }
+
+    public Branch(String id, String name, String address, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
