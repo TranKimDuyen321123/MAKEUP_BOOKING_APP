3315a7dc1402018f5f83ce70d86bd46fc45cf585
Tranhuynh234 <tranhuynh23042005@gmail.com>
2025-03-31 21:47:49 +0700

    Update EmailService.java

--- src/main/java/com/example/makeup_booking_app/EmailService.java
+++ src/main/java/com/example/makeup_booking_app/EmailService.java
@@ -13,24 +13,26 @@ public class EmailService {
     }
 
     public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        } catch (Exception e) {
+            System.err.println("Error sending email: " + e.getMessage());
+        }
     }
 
-    // Gửi email xác nhận đặt lịch
     public void sendBookingConfirmation(String to, String bookingDetails) {
         String subject = "Xác nhận đặt lịch Makeup";
-        String body = "Chào bạn,\n\nLịch hẹn của bạn đã được xác nhận!\n\n" + bookingDetails + "\n\nCảm ơn bạn đã sử dụng dịch vụ của chúng tôi!";
+        String body = String.format("Chào bạn,\n\nLịch hẹn của bạn đã được xác nhận!\n\n%s\n\nCảm ơn bạn đã sử dụng dịch vụ của chúng tôi!", bookingDetails);
         sendEmail(to, subject, body);
     }
 
-    // Gửi email trạng thái thanh toán
     public void sendPaymentStatus(String to, String status) {
         String subject = "Cập nhật trạng thái thanh toán";
-        String body = "Chào bạn,\n\nTrạng thái thanh toán của bạn: " + status + "\n\nCảm ơn bạn đã sử dụng dịch vụ!";
+        String body = String.format("Chào bạn,\n\nTrạng thái thanh toán của bạn: %s\n\nCảm ơn bạn đã sử dụng dịch vụ!", status);
         sendEmail(to, subject, body);
     }
 }
