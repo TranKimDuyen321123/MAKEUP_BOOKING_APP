e0b85d8e14f2bb315aed0c2e393a504d421b2612
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-01 23:03:42 +0700

    Update Branch.java

--- Branch.java
+++ Branch.java
@@ -14,10 +14,10 @@ public class Branch {
     @Column(name = "name", nullable = false, length = 100)
     private String name;
 
-    @Column(name = "address", nullable = false, length = 200)
+    @Column(name = "address", nullable = false, length = 255)
     private String address;
 
-    @Column(name = "phone", length = 10)
+    @Column(name = "phone", length = 15)
     private String phone;
 
     @Column(name = "created_at", nullable = false, updatable = false,
