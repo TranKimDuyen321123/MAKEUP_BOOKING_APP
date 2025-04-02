bb8af5427d6ab45013a92518d2cf5f8965131181
Tranhuynh234 <tranhuynh23042005@gmail.com>
2025-04-02 21:37:04 +0700

    Update UserRepository.java

--- src/main/java/com/example/makeup_booking_app/UserRepository.java
+++ src/main/java/com/example/makeup_booking_app/UserRepository.java
@@ -3,9 +3,14 @@ package com.example.makeup_booking_app.repositories;
 import com.example.makeup_booking_app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;     
+import org.springframework.data.domain.Pageable; 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email); // Tìm khách hàng bằng email
+
+    // Phương thức tìm kiếm với phân trang
+    Page<User> findByNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String name, String email, Pageable pageable);
 }
