dbba0233a87e7c96d834687db11ec357e0b6227e
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-12 19:39:41 +0700

    Create AdminDashboardService.java

---
+++ src/main/java/com/example/makeup_booking_app/service/AdminDashboardService.java
@@ -0,0 +1,7 @@
+package com.example.makeup_booking_app.service;
+
+import com.example.makeup_booking_app.dto.AdminDashboardData;
+
+public interface AdminDashboardService {
+    AdminDashboardData getAdminDashboardData();
+}
