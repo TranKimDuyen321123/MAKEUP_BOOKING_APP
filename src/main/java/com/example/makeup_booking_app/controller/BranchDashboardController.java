2d6af1014f9bbfd283b0648c65677596fb8c0fb6
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-12 19:30:03 +0700

    Add files via upload

---
+++ src/main/java/com/example/makeup_booking_app/controller/BranchDashboardController.java
@@ -0,0 +1,25 @@
+package com.example.makeup_booking_app.controller;
+
+import com.example.makeup_booking_app.dto.BranchDashboardData;
+import com.example.makeup_booking_app.service.BranchDashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/branch")
+public class BranchDashboardController {
+
+    @Autowired
+    private BranchDashboardService branchDashboardService;
+
+    @GetMapping("/{branchId}/dashboard-data")
+    public ResponseEntity<BranchDashboardData> getBranchDashboardData(@PathVariable Long branchId) {
+        BranchDashboardData dashboardData = branchDashboardService.getDashboardData(branchId);
+        if (dashboardData != null) {
+            return ResponseEntity.ok(dashboardData);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
