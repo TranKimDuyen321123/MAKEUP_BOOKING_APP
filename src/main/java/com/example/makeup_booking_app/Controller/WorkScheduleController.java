b79fb0e112f0674b3fef44de89750cc947911086
ThienTru1612 <trult3459@ut.edu.vn>
2025-04-12 11:20:20 +0700

    Update WorkScheduleController.java

--- src/main/java/com/example/makeup_booking_app/Controller/WorkScheduleController.java
+++ src/main/java/com/example/makeup_booking_app/Controller/WorkScheduleController.java
@@ -1,8 +1,27 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.makeup_booking_app.Entity.WorkSchedule;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import com.example.makeup_booking_app.Services.WorkScheduleService;
+import com.example.makeup_booking_app.Entity.WorkSchedule;
 @RestController
 @RequestMapping("/api/schedules")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class WorkScheduleController {
     private final WorkScheduleService service;
+
+    @Autowired
+    public WorkScheduleController(WorkScheduleService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public ResponseEntity<List<WorkSchedule>> getAll() {
