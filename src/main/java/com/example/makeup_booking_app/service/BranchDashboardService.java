79fd046b6572f2a446e87984a01a7114082efa4a
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-12 19:41:41 +0700

    Add files via upload

---
+++ src/main/java/com/example/makeup_booking_app/service/BranchDashboardService.java
@@ -0,0 +1,7 @@
+package com.example.makeup_booking_app.service;
+
+import com.example.makeup_booking_app.dto.BranchDashboardData;
+
+public interface BranchDashboardService {
+    BranchDashboardData getDashboardData(Long branchId);
+}
