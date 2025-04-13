d131ab56fe7ca352b186211cb2261eaa9207ac97
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-13 21:13:43 +0700

    Add files via upload

---
+++ src/main/java/com/example/makeup_booking_app/model/MakeupService.java
@@ -0,0 +1,106 @@
+package com.example.makeup_booking_app.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "services")
+public class MakeupService {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category", nullable = false)
+    private Category category;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
+    public MakeupService() {
+    }
+
+    public MakeupService(Integer id, String name, String description, Category category, BigDecimal price, Integer duration) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return "MakeupService{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", duration=" + duration +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public enum Category{
+        Bridal, Casual, Party, Skincare
+    }
+}
