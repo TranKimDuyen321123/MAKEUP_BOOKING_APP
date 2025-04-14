4eccc3d760527e8eae54b91ebc3b61c9b7cb0095
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-14 23:20:15 +0700

    Add files via upload

---
+++ src/main/java/com/example/makeup_booking_app/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.example.makeup_booking_app.controller;
+
+import com.example.makeup_booking_app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.makeup_booking_app.UserService;
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
