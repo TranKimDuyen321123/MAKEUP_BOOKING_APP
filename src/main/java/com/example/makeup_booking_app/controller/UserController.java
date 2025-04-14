85f0065999f9dd6f6ff363112609bc87c91ca322
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-14 23:45:55 +0700

    Update UserController.java

--- src/main/java/com/example/makeup_booking_app/controller/UserController.java
+++ src/main/java/com/example/makeup_booking_app/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.makeup_booking_app.UserService;
+import com.example.makeup_booking_app.service.UserService;
 @RestController
 @RequestMapping("/user")
 public class UserController {
