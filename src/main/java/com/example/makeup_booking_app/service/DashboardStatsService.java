14611c965b9da574d05ca2967ba86860179e7755
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-16 02:56:02 +0700

    Add files via upload

---
+++ src/main/java/com/example/makeup_booking_app/service/DashboardStatsService.java
@@ -0,0 +1,7 @@
+package com.example.makeup_booking_app.service;
+
+import com.example.makeup_booking_app.dto.SystemStatsResponse;
+
+public interface DashboardStatsService {
+    SystemStatsResponse getSystemStats();
+}
