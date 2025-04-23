066186beb3573b6d2c68e561dc84c3d6f320b3a0
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-23 22:42:58 +0700

    Update BranchDashboardService.java

--- src/main/java/com/example/makeup_booking_app/service/BranchDashboardService.java
+++ src/main/java/com/example/makeup_booking_app/service/BranchDashboardService.java
@@ -3,5 +3,5 @@ package com.example.makeup_booking_app.service;
 import com.example.makeup_booking_app.dto.BranchDashboardData;
 
 public interface BranchDashboardService {
-    BranchDashboardData getDashboardData(String branchId);
+    BranchDashboardData getDashboardData(Long branchId);
 }
