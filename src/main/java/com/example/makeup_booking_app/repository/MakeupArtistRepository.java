dfed81d3f58a7e86bef1e2b3279e522c3068ee53
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-23 22:36:04 +0700

    Update MakeupArtistRepository.java

--- src/main/java/com/example/makeup_booking_app/repository/MakeupArtistRepository.java
+++ src/main/java/com/example/makeup_booking_app/repository/MakeupArtistRepository.java
@@ -2,10 +2,14 @@ package com.example.makeup_booking_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 import com.example.makeup_booking_app.model.MakeupArtist;
+import java.util.List;
 
 @Repository
-public interface MakeupArtistRepository extends JpaRepository<MakeupArtist, String> {
-    List<MakeupArtist> findByBranch_BranchId(String branchId); // Sử dụng branch.branchId để truy cập
+public interface MakeupArtistRepository extends JpaRepository<MakeupArtist, Long> {
+    List<MakeupArtist> findByBranch_Id(Long branchId);
+
+    List<MakeupArtist> findByUser_Id(Long userId);
+
+    List<MakeupArtist> findBySpecialtyContaining(String keyword);
 }
