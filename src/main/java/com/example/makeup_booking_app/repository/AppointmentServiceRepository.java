9a687aebc4b661d79477056a7bae371ada016e71
hoangyen0202 <yenvh8527@ut.edu.vn>
2025-04-23 22:35:26 +0700

    Update AppointmentServiceRepository.java

--- src/main/java/com/example/makeup_booking_app/repository/AppointmentServiceRepository.java
+++ src/main/java/com/example/makeup_booking_app/repository/AppointmentServiceRepository.java
@@ -1,13 +1,15 @@
 package com.example.makeup_booking_app.repository;
 
+import com.example.makeup_booking_app.model.AppointmentService;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query; // Import này
-import org.springframework.data.repository.query.Param; // Import này
-import java.util.List;  // Import này
-import com.example.makeup_booking_app.AppointmentService;
+import org.springframework.stereotype.Repository;
 
-public interface AppointmentServiceRepository extends JpaRepository<AppointmentService, String> {
-    // Các phương thức khác
-    @Query("SELECT ms.name, COUNT(aps.makeupService.id) FROM AppointmentService aps JOIN aps.makeupService ms WHERE aps.appointment.branch.id = :branchId GROUP BY ms.name")
-    List<Object[]> countServicesByBranchId(@Param("branchId") String branchId);
+import java.util.List;
+
+@Repository
+public interface AppointmentServiceRepository extends JpaRepository<AppointmentService, Long> {
+
+    List<AppointmentService> findByAppointment_Id(Long appointmentId);
+
+    List<AppointmentService> findByMakeupService_Id(Long makeupServiceId);
 }
